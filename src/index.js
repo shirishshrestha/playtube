105a9c316729ef3479ed856909a99b0d2b6b3fbb
SHIRISH <shirish.shrestha07@gmail.com>
2024-05-20 09:52:46 +0545

    utils: apierror, apirespsonse, asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("App is running on port: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed !!!", error);
+  });
 
 // import express from "express";
 // const app = express();
